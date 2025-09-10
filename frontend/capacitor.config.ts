0134d4728d8588ef2fa1a98a7c9e6798a1bc0159
Yeremi Guerrero <131369265+YeremG@users.noreply.github.com>
2025-09-10 03:12:31 -0300

    Delete frontend directory

--- frontend/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.nexa.app',
-  appName: 'Nexa',
-  webDir: 'dist'
-};
-
-export default config;
