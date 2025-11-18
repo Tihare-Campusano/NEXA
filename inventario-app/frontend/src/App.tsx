27e1692419ac1faaf8260a6d02749824dbeb16c4
YeremGA <yeremiguerrero2001@gmail.com>
2025-11-17 22:30:14 -0300

    Se arregla error de App.tsx

--- inventario-app/frontend/src/App.tsx
+++ inventario-app/frontend/src/App.tsx
@@ -43,55 +43,86 @@ import Perfil from "./pages/perfil/perfil";
 
 /* Páginas de Registro */
 import RegisterManual from "./pages/register-product/register-manual";
+import ScannerGun from "./pages/register-product/registro_pistola";
+import ScannerCamera from "./pages/register-product/scanner-camera";
+import IAImagen from "./pages/register-product/ia-images/ia-images";
+import { ThemeProvider } from "./contexts/ThemeContext";
+
+setupIonicReact();
 
 const App: React.FC = () => {
+
+  StatusBar.setOverlaysWebView({ overlay: false });
+  StatusBar.setStyle({ style: Style.Dark });  // texto claro (para barra oscura)
+  StatusBar.setStyle({ style: Style.Light }); // texto oscuro (para barra clara)
+
   return (
-    <IonApp>
-      <IonReactRouter>
-        <IonTabs>
+    <ThemeProvider>
+      <IonApp>
+        <IonReactRouter>
           <IonRouterOutlet>
-            {/* Rutas principales */}
-            <Route exact path="/" component={Home} />
-            <Route exact path="/productos" component={Productos} />
-            <Route exact path="/editor-producto" component={EditorProducto} />
-            <Route exact path="/reportes" component={Reportes} />
-            <Route exact path="/perfil" component={Perfil} />
-            <Route exact path="/register-manual" component={RegisterManual} />
-
-            {/* Autenticación */}
+            {/* Rutas Públicas */}
+            <Route exact path="/" render={() => <Redirect to="/login" />} />
             <Route exact path="/login" component={Login} />
-            <Route exact path="/auth/callback" component={AuthCallback} />
+            <Route exact path="/auth/callback" component={AuthCallback} /> 
+            
+            {/* Rutas con Tabs */}
+            <Route path="/tabs" render={() => <TabsLayout />} />
 
-            {/* Redirección por defecto */}
-            <Redirect to="/" />
           </IonRouterOutlet>
+        </IonReactRouter>
 
-          {/* Tabs */}
-          <IonTabBar slot="bottom">
-            <IonTabButton tab="home" href="/">
-              <IonIcon icon={home} />
-              <IonLabel>Inicio</IonLabel>
-            </IonTabButton>
+      </IonApp>
 
-            <IonTabButton tab="productos" href="/productos">
-              <IonIcon icon={list} />
-              <IonLabel>Productos</IonLabel>
-            </IonTabButton>
+    </ThemeProvider>
 
-            <IonTabButton tab="reportes" href="/reportes">
-              <IonIcon icon={barChart} />
-              <IonLabel>Reportes</IonLabel>
-            </IonTabButton>
-
-            <IonTabButton tab="perfil" href="/perfil">
-              <IonIcon icon={person} />
-              <IonLabel>Perfil</IonLabel>
-            </IonTabButton>
-          </IonTabBar>
-        </IonTabs>
-      </IonReactRouter>
-    </IonApp>
   );
 };
+/* Componente para el Layout de las Rutas con Tabs */
+
+const TabsLayout: React.FC = () => (
+  <IonTabs>
+    <IonRouterOutlet>
+      <Route exact path="/tabs" render={() => <Redirect to="/tabs/home" />} />
+
+      {/* Rutas con Tabs */}
+      <Route exact path="/tabs/home" component={Home} />
+      <Route exact path="/tabs/reportes" component={Reportes} />
+      <Route exact path="/tabs/productos" component={Productos} />
+      <Route exact path="/tabs/perfil" component={Perfil} />
+
+      {/* Rutas de Registro */}
+      <Route exact path="/tabs/registro" component={RegisterManual} />
+      <Route exact path="/tabs/registro/pistola" component={ScannerGun} />
+      <Route exact path="/tabs/registro/camera" component={ScannerCamera} />
+      <Route exact path="/tabs/registro/ia" component={IAImagen} />
+
+      {/* Ruta interna con Tabs para detalle de producto */}
+      <Route exact path="/tabs/product/:id" component={EditorProducto} />
+    </IonRouterOutlet>
+    <IonTabBar slot="bottom">
+      <IonTabButton tab="home" href="/tabs/home">
+        <IonIcon icon={home} />
+        <IonLabel>Inicio</IonLabel>
+      </IonTabButton>
+      <IonTabButton tab="reportes" href="/tabs/reportes">
+        <IonIcon icon={barChart} />
+        <IonLabel>Reportes</IonLabel>
+      </IonTabButton>
+      <IonTabButton tab="registro" href="/tabs/registro">
+        <IonIcon icon={create} />
+        <IonLabel>Registrar</IonLabel>
+      </IonTabButton>
+      <IonTabButton tab="productos" href="/tabs/productos">
+        <IonIcon icon={list} />
+        <IonLabel>Productos</IonLabel>
+      </IonTabButton>
+      <IonTabButton tab="perfil" href="/tabs/perfil">
+        <IonIcon icon={person} />
+        <IonLabel>Perfil</IonLabel>
+      </IonTabButton>
+    </IonTabBar>
+  </IonTabs>
+);
 
 export default App;
