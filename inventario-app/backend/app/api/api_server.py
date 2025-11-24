e478275f10317bfce5b0edef6f540128bce12335
tih4r3 <tb.campusano@gmail.com>
2025-11-24 11:41:43 -0300

    Update api_server.py

--- inventario-app/backend/app/api/api_server.py
+++ inventario-app/backend/app/api/api_server.py
@@ -26,7 +26,8 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    # allow_credentials=True,
+    allow_origin_regex=".*", 
     allow_methods=["*"],
     allow_headers=["*"],
 )
